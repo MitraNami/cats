fabab627ee4fbd61ba788ccb4fcd484a74b5dbb9
Mitra Nami <a.mxnami@gmail.com>
2020-10-21 02:17:34 +0000

    export the function for unit testing

--- asyncBreeds.js
+++ asyncBreeds.js
@@ -1,14 +1,19 @@
 const fs = require('fs');
 
-const breedDetailsFromFile = function(breed, done) {
+const breedDetailsFromFile = function(breed, Callback) {
   console.log('breedDetailsFromFile: Calling readFile...');
   fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
     console.log("In readFile's Callback: it has the data.");
-  
-    if (!error) done(data);
+    if (!error) {
+      Callback(data);
+      return;
+    }
+    Callback(undefined);
   });
 };
 
 
 
-breedDetailsFromFile('Bombay', breedInfo => console.log("Return value: " , breedInfo));
+// breedDetailsFromFile('Bombay', breedInfo => console.log("Return value: " , breedInfo));
+
+module.exports = breedDetailsFromFile;
